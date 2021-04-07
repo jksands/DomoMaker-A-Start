4631ddfbfdb990c75c7aa18ed9cfe541bed84cef
jksands <jksands83@gmail.com>
2021-04-07 12:59:59 -0400

    Finished Part B

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
+const session = require('express-session');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
@@ -14,16 +15,16 @@ const dbURL = 'mongodb+srv://jxs4264:8DDXbidLp7PE!Sj@cluster0.om6bx.mongodb.net/
 
 // Setup mongoose options to use newer functionality
 const mongooseOptions = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-}
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+};
 
 mongoose.connect(dbURL, mongooseOptions, (err) => {
-    if (err) {
-        console.log('Could not connect to databse');
-        throw err;
-    }
+  if (err) {
+    console.log('Could not connect to databse');
+    throw err;
+  }
 });
 
 // pull in our routes
@@ -34,18 +35,24 @@ app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.use(compression());
 app.use(bodyParser.urlencoded({
-    extended: true,
+  extended: true,
 }));
-app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}));
+app.use(session({
+  key: 'sessionid',
+  secret: 'Domo Arigato',
+  resave: true,
+  saveUninitialized: true,
+}));
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
 app.use(cookieParser());
 
 router(app);
 
-app.listen(port, (err) =>{
-    if (err) {
-        throw err;
-    }
-    console.log(`Listening on port ${port}`);
+app.listen(port, (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log(`Listening on port ${port}`);
 });
